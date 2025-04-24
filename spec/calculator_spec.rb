4078d555cb57e3c1fd80fbfebecdbdce53f6645f
Rohit Kumar Rana <rohit@engagedly.com>
2025-04-25 00:49:03 +0530

    added test case context 'with invalid argument' with test 'should raise ArgumentError'

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -1,4 +1,9 @@
 require 'calculator'
 
 RSpec.describe Calculator do
+  context 'with invalid argument' do
+    it 'should raise ArgumentError' do
+      expect { Calculator.add(123) }.to raise_error(ArgumentError, 'Input must be a String')
+    end
+  end
 end
