3faba5d9e5831f5f3e7eaf08b0e4d432aa589139
Rohit Kumar Rana <rohit@engagedly.com>
2025-04-25 10:25:57 +0530

    added code to pass test case for context 'with one number'

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -2,6 +2,10 @@ class Calculator
   def self.add(numbers)
     raise ArgumentError, 'Input must be a String' unless numbers.is_a?(String)
 
-    0 if numbers.strip.empty?
+    return 0 if numbers.strip.empty?
+
+    nums_array = numbers.split(',')
+    nums_array[0].to_i if nums_array.size == 1
+
   end
 end
