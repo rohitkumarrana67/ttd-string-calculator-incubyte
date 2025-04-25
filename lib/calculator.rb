102101ca3cb3ff9cd1b4b185d4e129f8a3dc6f07
Rohit Kumar Rana <rohit@engagedly.com>
2025-04-25 15:36:12 +0530

    added code to pass test case context  'with new lines between the numbers'

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -4,10 +4,10 @@ class Calculator
 
     return 0 if numbers.strip.empty?
 
-    nums_array = numbers.split(',')
+    nums_array = numbers.gsub("\n", ",").split(',')
     return nums_array[0].to_i if nums_array.size == 1
 
-    nums_array = numbers.split(',').map { |num| num.strip.to_i }
+    nums_array = nums_array.map { |num| num.strip.to_i }
     nums_array.sum
 
   end
