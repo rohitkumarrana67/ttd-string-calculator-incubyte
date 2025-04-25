9dcfb473937f74116e237802824e369b5de1d42b
Rohit Kumar Rana <rohit@engagedly.com>
2025-04-25 15:54:53 +0530

    added code to pass test case context 'with different delimiters'

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -4,8 +4,13 @@ class Calculator
 
     return 0 if numbers.strip.empty?
 
-    nums_array = numbers.gsub("\n", ",").split(',')
-    return nums_array[0].to_i if nums_array.size == 1
+    delimiter = ","
+    if numbers.start_with?("//")
+      delimiter_line, numbers = numbers.split("\n", 2)
+      delimiter = delimiter_line[2..-1]
+    end
+
+    nums_array = numbers.split(/#{Regexp.escape(delimiter)}|\n/)
 
     nums_array = nums_array.map { |num| num.strip.to_i }
     nums_array.sum
